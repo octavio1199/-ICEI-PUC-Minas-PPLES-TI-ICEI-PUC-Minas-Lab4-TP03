77d6d01e394469f6d0c1bb38c53bc81643821a00
Vítor José <83792704+vitorjoseph@users.noreply.github.com>
2022-10-05 02:39:26 -0300

    Add files via upload

---
+++ Implementação/model/Professor.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Professor extends Usuario {
+
+	private int moedas;
+
+	private String cpf;
+
+	private String departamento;
+
+	public void adquirirMoedas(int moedas) {
+
+	}
+
+	public void mandarMoedas(int moedas, Aluno aluno) {
+
+	}
+
+}
---
+++ Implementação/model/Vantagem.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Vantagem {
+
+	private int quantidade;
+
+	private String descricao;
+
+}
